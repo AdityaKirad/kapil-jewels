909a784057263d9f851aa281276f58f79d8d7922
Aditya <kiradaditya2001@gmail.com>
2025-06-04 12:06:26 +0530

    feat: add @neondatabase/serverless dependency and update drizzle ORM integration

--- src/server/drizzle/index.ts
+++ src/server/drizzle/index.ts
@@ -1,14 +1,8 @@
+import { neon } from "@neondatabase/serverless";
 import { env } from "~/env";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const globalForClient = globalThis as unknown as {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  client: postgres.Sql<{}> | undefined;
-};
+const sql = neon(env.DATABASE_URL);
 
-const client = globalForClient.client ?? postgres(env.DATABASE_URL);
-
-if (env.NODE_ENV !== "production") globalForClient.client = client;
-export const db = drizzle(client, { schema, logger: true });
+export const db = drizzle(sql, { schema, logger: true });
